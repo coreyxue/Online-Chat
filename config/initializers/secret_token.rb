1a5caefced2e3e390cee60e67e4525f14fd1aaf1
coreyxue <coreyxueatcal@gmail.com>
2013-12-31 08:23:23 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineChat::Application.config.secret_token = '6daf176008a7a2356f93e8d778a7883e5c927effa7b0c1ac4a468dcf59fd758e5936190c3b5ea3c96219a4e3647505ec3f107f5ac7df31c0224fcd507d113d40'
